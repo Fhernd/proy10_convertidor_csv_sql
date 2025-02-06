8d368df18a5ed12a747ef23d06c19625b76d701e
John Ortiz Ordoñez <johnortizo@outlook.com>
2025-02-06 13:11:25 -0500

    Remoción de componente duplicado.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,22 +5,7 @@
 <template>
   <main>
     <h2>App</h2>
-    <Tabs>
-      <template #default="{ activeTab }">
-        <Tab label="Pestaña 1">
-          <h2>Contenido de Pestaña 1</h2>
-          <p>Este es el contenido de la primera pestaña.</p>
-        </Tab>
-        <Tab label="Pestaña 2">
-          <h2>Contenido de Pestaña 2</h2>
-          <p>Este es el contenido de la segunda pestaña.</p>
-        </Tab>
-        <Tab label="Pestaña 3">
-          <h2>Contenido de Pestaña 3</h2>
-          <p>Este es el contenido de la tercera pestaña.</p>
-        </Tab>
-      </template>
-    </Tabs>
+
   </main>
 </template>
 
