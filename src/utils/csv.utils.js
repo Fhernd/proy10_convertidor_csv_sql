08f968e04a7d9ddbaad08f2b562774184b64181c
John Ortiz Ordoñez <johnortizo@outlook.com>
2025-02-20 18:47:29 -0500

    Función que permite detectar el delimintador de un contenido en formato CSV. Se hace uso de la librería papaparse.

---
+++ src/utils/csv.utils.js
@@ -0,0 +1,27 @@
+import Papa from 'papaparse';
+
+/**
+ * Detecta el delimitador de un archivo CSV.
+ * 
+ * @param {*} muestraCsv Cadena de texto de muestra con el contenido del archivo CSV.
+ * @returns Delimitador detectado.
+ */
+function detectarDelimitador(muestraCsv) {
+    const delimiters = [',', ';', '\t', '|', '^', '~', ':'];
+
+    let detectedDelimiter = null;
+    let maxFields = 0;
+
+    delimiters.forEach(delimiter => {
+        const parsed = Papa.parse(muestraCsv, { delimiter, preview: 5 });
+        if (parsed.data.length > 0) {
+            const fieldCount = parsed.data[0].length;
+            if (fieldCount > maxFields) {
+                maxFields = fieldCount;
+                detectedDelimiter = delimiter;
+            }
+        }
+    });
+
+    return detectedDelimiter || ',';
+}
