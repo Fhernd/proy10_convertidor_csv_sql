2d877dcb43c5e837a910929df63c401efb520a07
John Ortiz Ordoñez <johnortizo@outlook.com>
2025-03-02 19:24:52 -0500

    Arreglos con los tipos de datos de los SGBD más comunes: MySQL, PostgreSQL, SQLite y SQL Server.

--- src/components/OpcionesSalida.vue
+++ src/components/OpcionesSalida.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script setup>
-import {defineProps, ref } from "vue";
+import { defineProps, ref } from "vue";
 
 import OpcionesSalidaColumnas from "./OpcionesSalidaColumnas.vue";
 
@@ -17,4 +17,135 @@ const props = defineProps({
 const columnas = ref(props.columnas);
 const sgbdSeleccionado = ref(props.sgbdSeleccionado);
 const tiposDatosSgbd = ref([]);
+
+const mysqlDataTypes = [
+    "INT",
+    "VARCHAR",
+    "TEXT",
+    "DATETIME",
+    "TINYINT",
+    "DECIMAL",
+    "BIGINT",
+    "BOOLEAN",
+    "FLOAT",
+    "DOUBLE",
+    "TIMESTAMP",
+    "DATE",
+    "SMALLINT",
+    "MEDIUMINT",
+    "CHAR",
+    "BLOB",
+    "ENUM",
+    "SET",
+    "TIME",
+    "YEAR",
+    "VARBINARY",
+    "BINARY",
+    "TINYTEXT",
+    "MEDIUMTEXT",
+    "LONGTEXT",
+    "TINYBLOB",
+    "MEDIUMBLOB",
+    "LONGBLOB",
+    "NUMERIC"
+];
+
+const postgresDataTypes = [
+    "BIGINT",
+    "BIT",
+    "BOOLEAN",
+    "CHAR",
+    "DATE",
+    "DECIMAL",
+    "DOUBLE PRECISION",
+    "ENUM",
+    "FLOAT",
+    "INTEGER",
+    "JSON",
+    "JSONB",
+    "MACADDR",
+    "MACADDR8",
+    "MONEY",
+    "NUMERIC",
+    "REAL",
+    "SMALLINT",
+    "TEXT",
+    "TIME",
+    "TIMESTAMP",
+    "TIMESTAMPTZ",
+    "TIMETZ",
+    "UUID",
+    "XML"
+];
+
+const sqliteDataTypes = [
+    "INT",
+    "INTEGER",
+    "TINYINT",
+    "SMALLINT",
+    "MEDIUMINT",
+    "BIGINT",
+    "UNSIGNED BIG INT",
+    "INT2",
+    "INT8",
+    "CHARACTER(20)",
+    "VARCHAR(255)",
+    "VARYING CHARACTER(255)",
+    "NCHAR(55)",
+    "NATIVE CHARACTER(70)",
+    "NVARCHAR(100)",
+    "TEXT",
+    "CLOB",
+    "BLOB",
+    "REAL",
+    "DOUBLE",
+    "DOUBLE PRECISION",
+    "FLOAT",
+    "NUMERIC",
+    "DECIMAL(10,5)",
+    "BOOLEAN",
+    "DATE",
+    "DATETIME"
+];
+
+const mssqlDataTypes = [
+    "BIGINT",
+    "INT",
+    "SMALLINT",
+    "TINYINT",
+    "BIT",
+    "DECIMAL",
+    "NUMERIC",
+    "MONEY",
+    "SMALLMONEY",
+    "FLOAT",
+    "REAL",
+    "DATE",
+    "DATETIME",
+    "DATETIME2",
+    "DATETIMEOFFSET",
+    "SMALLDATETIME",
+    "TIME",
+    "CHAR",
+    "VARCHAR",
+    "TEXT",
+    "NCHAR",
+    "NVARCHAR",
+    "NTEXT",
+    "BINARY",
+    "VARBINARY",
+    "IMAGE",
+    "ROWVERSION",
+    "TIMESTAMP",
+    "UNIQUEIDENTIFIER",
+    "XML",
+    "CURSOR",
+    "TABLE",
+    "HIERARCHYID",
+    "GEOMETRY",
+    "GEOGRAPHY",
+    "SQL_VARIANT",
+    "SYSNAME"
+];
+
 </script>
