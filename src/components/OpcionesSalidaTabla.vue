f2904ecd2242bd4090972d76962879e1a1580076
John Ortiz Ordoñez <johnortizo@outlook.com>
2025-03-30 11:22:03 -0500

    Inclusión del campo que permite especificar el nombre de la tabla para las expresiones INSERT que se generarán al final

--- src/components/OpcionesSalidaTabla.vue
+++ src/components/OpcionesSalidaTabla.vue
@@ -3,6 +3,12 @@
         <h2 class="text-xl font-bold mb-4">Opciones de Salida de la Tabla</h2>
 
         <div class="flex flex-col space-y-4">
+            <div class="mb-4">
+                <label for="table-name" class="block text-gray-700 font-bold mb-2">Nombre de la Tabla:</label>
+                <input id="table-name" v-model="tableName" type="text" placeholder="Introduce el nombre de la tabla"
+                    class="w-full p-3 border border-gray-300 rounded focus:ring focus:ring-blue-200">
+            </div>
+
             <label class="flex items-center">
                 <input v-model="replaceSpaces" type="checkbox" class="mr-2 h-5 w-5 text-blue-600 border-gray-300 rounded focus:ring focus:ring-blue-200">
                 Sustitución de espacios por guiones bajos en nombres de columnas
@@ -34,6 +40,7 @@
 <script setup>
 import { defineEmits, ref, watch } from 'vue';
 
+const tableName = ref('datos');
 const replaceSpaces = ref(false);
 const allVarchar = ref(false);
 const createView = ref(false);
@@ -44,6 +51,7 @@ const emit = defineEmits(['update:opcionesSalidaTabla']);
 
 const applyOptions = () => {
     const options = {
+        tableName: tableName.value,
         replaceSpaces: replaceSpaces.value,
         allVarchar: allVarchar.value,
         createView: createView.value,
@@ -54,7 +62,7 @@ const applyOptions = () => {
     emit('update:opcionesSalidaTabla', options);
 };
 
-watch([replaceSpaces, allVarchar, createView, dropTable, selectPrimaryKey], () => {
+watch([tableName, replaceSpaces, allVarchar, createView, dropTable, selectPrimaryKey], () => {
     applyOptions();
 }, { deep: true });
 </script>
